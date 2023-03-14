7faa9811d14fbb1e4e3d0e21527a1b43eda3b741
JacopoGrottadaurea <119618610+JacopoGrottadaurea@users.noreply.github.com>
2023-03-14 19:10:39 +0100

    Add files via upload

---
+++ Netflix CLONE/logic.js
@@ -0,0 +1,24 @@
+// Cambia "Netflix Time?" con "Home".
+
+function home() {
+    document.getElementById("change").innerHTML = "Home";
+}
+
+// Cambia "Netflix Time?" con "TV Shows".
+
+function tvShows() {
+    document.getElementById("change").innerHTML = "TV Shows";
+}
+
+// Cambia "Netflix Time?" con "Movies".
+
+function movies() {
+    document.getElementById("change").innerHTML = "Movies";
+}
+
+// Attiva l'animazione "slideIn" quando viene premuta l'icona.
+
+function slideIn() {
+    document.getElementById("search-bar-box").id = "ani-search-bar-box-open";
+}
+
